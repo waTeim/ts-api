e3b011ce58856957a79e1cfd3b9cadf539d3d3fe
Taras Mitran <taras@ivc.com>
2018-11-10 19:34:40 -0500

    fix typo

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const index = require('../dist/index.js');
-describe('hello wolrd', () => {
+describe('hello world', () => {
     it('should return true', () => {
         expect(true).to.equal(true);
     });
